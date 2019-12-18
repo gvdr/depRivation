a984dca623933c6b8dec64e6cf3cab27aa1e1f2f
Giulio Valentino Dalla Riva￼ <me@gvdallariva.net>
2019-12-18 17:29:52 +1300

    AreaUnits docus

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' NZ_AreaUnits
+#'
+#' New Zealand Indexes of Deprivation
+#'
+#' @format A data frame with 5 variables:
+#' \describe{
+#' \item{\code{year}}{the year of the study}
+#' \item{\code{CAU_num}}{numeric Area Unit code}
+#' \item{\code{CAU_name}}{name of Area Unit}
+#' \item{\code{Indicator}}{kind of indicator: either "CAU_avg_NZD", "CAU_avg_NZD_score", "AUScore" or "AUScale"}
+#' \item{\code{Value}}{value of the indicator, as a numerical}
+#' }
+#'
+#' For further details, see \url{https://www.otago.ac.nz/wellington/departments/publichealth/research/hirp/otago020194.html}
+#'
+"NZ_AreaUnits"
